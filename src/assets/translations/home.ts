be514243c3557f4708aba206bde908de6ff00681
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-06 18:55:29 +0200

    configure basic i18n

---
+++ src/assets/translations/home.ts
@@ -0,0 +1,10 @@
+export const homeTranslations = {
+	bg: {
+		heading: 'Рекламна агенция Студио Дада',
+	},
+	en: {
+		heading: 'Advertisement agency Studio Dada',
+	},
+};
+
+export type HomeTranslationKeys = typeof homeTranslations.bg;
