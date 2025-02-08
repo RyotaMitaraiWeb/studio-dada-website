bf263f89e2767917dbd43ba83dfc44130d76966e
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-08 12:39:14 +0200

    upload all images

---
+++ src/translations/nav.ts
@@ -0,0 +1,19 @@
+export const navigationTranslations = {
+  bg: {
+    home: 'Начало',
+    catalogue: 'Каталог',
+    customers: 'Клиенти',
+    portfolio: 'Портфолио',
+    about: 'За нас',
+    contacts: 'Контакти'
+
+  },
+  en: {
+    home: 'Home',
+    catalogue: 'Catalogue',
+    customers: 'Customers',
+    portfolio: 'Portfolio',
+    about: 'About us',
+    contacts: 'Contacts'
+  }
+};
