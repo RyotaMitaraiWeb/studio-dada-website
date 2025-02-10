d5fe5dcb5818dd77422bddb81cbb3f90d781db47
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-10 21:33:27 +0200

    implement contacts page

---
+++ src/translations/contacts.ts
@@ -0,0 +1,16 @@
+export const contactsTranslations = {
+  bg: {
+    title: 'Контакти',
+    address: 'Адрес',
+    phone: 'Телефон',
+    email: 'Имейл',
+    workHours: 'Работно време',
+  },
+  en: {
+    title: 'Contacts',
+    address: 'Address',
+    phone: 'Phone number',
+    email: 'Email',
+    workHours: 'Working hours',
+  },
+} as const;
