c724370eafa4f05075195a9cc8680e29a8c4b968
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-14 00:14:22 +0200

    create prototype catalogue page

--- src/common/catalogue.ts
+++ src/common/catalogue.ts
@@ -23,5 +23,89 @@ export const catalogue: CatalogueItem[] = [
       description: 'Lorem ipsum dolor',
     },
     imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
-  }
+  },
+  {
+    id: 'posters-showcards-wobblers-shelftalkers',
+    bg: {
+      name: 'Плакати, шоу карти, уоблери, шелфтокъри',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Posters, show cards, wobblers, shelftalkers',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'product-packages',
+    bg: {
+      name: 'Продуктови опаковки (кутии, бандероли, етикети, стикери)',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Product packages (boxes, excise stamps, tags, stickers)',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'calendars-notebooks-sacks-envelopes',
+    bg: {
+      name: 'Календари, тефтери, торби, пликове',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Calendars, notebooks, sacks, envelopes',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'design-and-development-of-product-packages',
+    bg: {
+      name: 'Проектиране и дизайн на продуктови опаковки',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Design and development of product packages',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'design-and-adaption-of-advertising-materials',
+    bg: {
+      name: 'Дизайн и адаптация на рекламни материали',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Design and adaptation of advertising materials',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'design-and-adaption-of-web-banners-boxes-and-advertising-pages',
+    bg: {
+      name: 'Дизайн и адаптация на уеб банери, карета и рекламни страници',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Design and adaptation of web banners, boxes, and advertising pages',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
+  {
+    id: 'branding-of-advertising-souvenirs-clothes-tools-and-others',
+    bg: {
+      name: 'Брандиране на рекламни сувенири, дрехи, инструменти и други',
+      description: 'Lorem ipsum dolor'
+    },
+    en: {
+      name: 'Branding of advertising souvenirs, clothes, tools, and others',
+      description: 'Lorem ipsum dolor',
+    },
+    imageFileName: '3D_Brown-Sugar_Box_100x4g_0922.png'
+  },
 ]
---
+++ src/translations/catalogue.ts
@@ -0,0 +1,10 @@
+export const catalogueTranslations = {
+  bg: {
+    title: 'Продукти и услуги',
+    viewMore: 'Виж повече'
+  },
+  en: {
+    title: 'Products and services',
+    viewMore: 'Learn more'
+  }
+}
