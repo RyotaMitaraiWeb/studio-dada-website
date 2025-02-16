cb42d2ba5e78cdde4206182e91969c98bb60cac5
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-16 18:28:24 +0200

    add accessibility features

--- src/translations/portfolio.ts
+++ src/translations/portfolio.ts
@@ -2,9 +2,13 @@ export const portfolioTranslations = {
   bg: {
     title: 'Портфолио',
     explore: 'Разгледай',
+    close: 'Затвори модала',
+    fullScreen: 'Натисни за отваряне на изображението на пълен екран'
   },
   en: {
     title: 'Portfolio',
     explore: 'Explore',
+    close: 'Close the modal',
+    fullScreen: 'Click to open the image in full screen'
   },
 }
