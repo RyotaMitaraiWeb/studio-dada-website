29441c4efdeb5102b285ee08dc5a9f23dfd9a745
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-16 15:29:45 +0200

    create base portfolio category pages

--- src/common/portfolio.ts
+++ src/common/portfolio.ts
@@ -1,8 +1,17 @@
 export type category = 'packages' | 'sacks' | 'wobblers';
+export type PortfolioItemImage = {
+  imageFileName: string;
+  bg: {
+    caption: string;
+  },
+  en: {
+    caption: string;
+  }
+}
 
 export type PortfolioCategory = {
   category: category;
-  imageFileNames: string[];
+  images: PortfolioItemImage[];
   headerImage: string;
   bg: {
     name: string;
@@ -16,12 +25,20 @@ export const portfolioCategories: PortfolioCategory[] = [
   {
     category: "packages",
     headerImage: 'apivita_bee_radiant_cream_box.png',
-    imageFileNames: ['apivita_bee_radiant_cream_box.png'],
     bg: {
       name: "Опаковки"
     },
     en: {
       name: "Packages"
     },
+    images: [{
+      imageFileName: 'apivita_bee_radiant_cream_box.png',
+      bg: {
+        caption: 'Кутия за крем за кожа "Bee Radiant" на Apivita, Дермикос',
+      },
+      en: {
+        caption: 'Box for the "Bee Radiant" skin cream of Apivita, Dermicos',
+      }
+    }],
   }
 ];
