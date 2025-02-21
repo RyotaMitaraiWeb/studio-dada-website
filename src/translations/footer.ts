6e8bff6837ba027afae18c4f41738cb714aab29c
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-02-22 01:16:51 +0200

    instruction should not be capitalized

--- src/translations/footer.ts
+++ src/translations/footer.ts
@@ -4,7 +4,7 @@ export const footerTranslations = {
     copyright: () => `Студио Дада ЕООД @ ${new Date().getFullYear()}. Този сайт не използва бисквитки`,
     websiteDevelopedByLabel: 'Уебсайт разработен от',
     valentinKaradzhov: 'Валентин Караджов',
-    viewInGoogleMaps: 'Виж в Google Maps',
+    viewInGoogleMaps: 'виж в Google Maps',
     contacts: {
       label: 'Контакти',
       addressLabel: 'Адрес',
@@ -21,7 +21,7 @@ export const footerTranslations = {
     copyright: () => `Studio Dada LTD @ ${new Date().getFullYear()}. This site does not use cookies`,
     websiteDevelopedByLabel: 'Website developed by',
     valentinKaradzhov: 'Valentin Karadzhov',
-    viewInGoogleMaps: 'View in Google Maps',
+    viewInGoogleMaps: 'view in Google Maps',
     contacts: {
       label: 'Contacts',
       phoneLabel: 'Phone number',
